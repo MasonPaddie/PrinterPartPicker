274df4040355f3fc0ae38d56034af753628222d5
MasonPaddie <masonpaddie29@yahoo.com>
2022-08-23 19:31:46 -0500

    can add new printer

--- server.js
+++ server.js
@@ -3,18 +3,53 @@ const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 var printers = require('./models/printers.js')
-// const Printers = require('./models/printers.js');
-// const methodOverride = require('method-override')
-// app.use(express.static('public'));
-// app.use(express.json())
-// app.use(express.urlencoded({extended: true}))
-// app.use(methodOverride('_method'))
+const Printers = require('./models/printers.js');
+const methodOverride = require('method-override')
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 //Index
 app.get(`/printers`, ( req, res )=>{
     res.render('index.ejs', 
     {printers: printers});
 });
+  
+//New
+app.get(`/printers/new`, ( req, res )=>{
+    res.render('new.ejs', 
+    {});
+});
+
+//Create
+app.post('/printers', ( req, res )=>{
+    console.log(req.body)
+    printers.push(req.body)
+    res.redirect('/printers')
+});
+
+//Show
+app.get(`/printers/:index`, ( req, res )=>{
+    const index = req.params.index
+    res.render('showPrinters.ejs',
+    {printers: printers,
+        index: index
+    });
+});
+
+//Edit
+app.get(`/printers/:index/edit`, ( req, res )=>{
+    res.render('edit.ejs',
+    {printers: printers[req.params.index],
+    index: req.params.index});
+});
+
+//Update
+app.put('/printers/:index', (req, res) => { 
+    printers[req.params.index] = req.body 
+      res.redirect('/printers')
+})
   
 //Mongo Route
 app.post('/printers/', (req, res)=>{
@@ -29,29 +64,11 @@ app.post('/printers/', (req, res)=>{
     });
 });
   
-//Show
-app.get(`/printers/:index`, ( req, res )=>{
-    const index = req.params.index
-    console.log(printers)
-    res.render('showPrinters.ejs',
-    {printers: printers,
-        index: index
-    });
-});
-
-//Edit
-app.get(`/printers/:index/edit`, ( req, res )=>{
-    res.render('edit.ejs',
-    {printers: printers[req.params.index],
-    index: req.params.index});
-});
-  
-
 app.listen(port,() => {
     console.log('listening on port' , port)
 })
 
-//... and then farther down the file
+
 mongoose.connect('mongodb://127.0.0.1:27017/basiccrud', { useNewUrlParser: true});
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
